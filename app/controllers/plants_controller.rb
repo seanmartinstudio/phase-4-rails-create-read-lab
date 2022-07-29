246e4aaa9a95673cd3b1149acc7c72e61ebbec46
Sean Martin <sean@seanmartinstudio.com>
2022-07-29 15:59:03 -0700

    Complete

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,32 @@
 class PlantsController < ApplicationController
+
+    #GET /plants
+    def index 
+        plants = Plant.all 
+        render json: plants
+    end
+
+    #GET /plants/:id
+    def show 
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: { error: "Plant not found"}, status: :not_found
+        end
+    end
+
+    #POST /plants
+    def create 
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+    
+
 end
